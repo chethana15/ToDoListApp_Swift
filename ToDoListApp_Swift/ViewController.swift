f9bdea4308ad8b8269e6aa2215223ac004a2ca85
Chethana N P <chethana.np@cumulations.com>
2022-07-11 18:21:43 +0530

    Added tableview and its delegate and datasource functions, embedded navigation controller. Added task and entry view controller and add bar button item.

--- ToDoListApp_Swift/ViewController.swift
+++ ToDoListApp_Swift/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrayData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func addTapBarButton(_ sender: Any) {
+        
+    }
+}
+
+extension ViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = arrayData[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayData.count
+    }
+    
 }
 
